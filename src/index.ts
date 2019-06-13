e64a47758aa3cb93b13bd39cecc54acfd9d4a636
Nishchal Gautam <gautam.nishchal@gmail.com>
2019-06-13 22:38:32 +0700

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
+import * as Webhooks from "@octokit/webhooks";
+import {Application} from "probot";
+import {OnCallback} from "probot/lib/application";
 
-/**
- * Root module
- */
-export * from "./MyClass";
+const handleStatus: OnCallback<Webhooks.WebhookPayloadPullRequest> = async (context) => {
+    const {owner, name: repo} = context.payload.repository;
+    const num = context.payload.number;
 
-/**
- * Sub modules
- */
-// import * as Utils from "./utils/index";
-// export { Utils };
+    const data = await context.config("pr_labels.yml");
+    context.log.info(data, repo, owner, num);
+    context.log.info(context.payload.pull_request.labels);
+};
+
+const handler = (context: Application) => {
+    context.on("pull_request.opened", handleStatus);
+    context.on("pull_request.reopened", handleStatus);
+    context.on("pull_request.unlabeled", handleStatus);
+};
+// tslint:disable-next-line:no-default-export export-name
+export default handler;
