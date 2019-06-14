1f66d4a21abb52f2347fcb270d9097a21809ead5
Nishchal Gautam <gautam.nishchal@gmail.com>
2019-06-15 00:25:21 +0700

    WIP

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,6 @@ echo "setting up now variable"
 now="npx now --debug --token=$NOW_TOKEN"
 ${now} secret add webhook_secret ${WEBHOOK_SECRET}
 ${now} secret add client_secret ${CLIENT_SECRET}
-${now} secret add private_key ${PRIVATE_KEY}
+${now} secret add -- "private_key" "${PRIVATE_KEY}"
 echo "$ now --no-verify"
 ${now} --no-verify
