44fc19714f5e9543d0e37cbbefef3be49d2c8b11
Nishchal Gautam <gautam.nishchal@gmail.com>
2019-06-16 20:42:53 +0700

    WIP

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,4 +6,4 @@ echo "Deploying application"
 echo "setting up now variable"
 now="npx now --debug --token=$NOW_TOKEN"
 echo "$ now --no-verify"
-${now}
+${now} -e APP_ID=${APP_ID} -e WEBHOOK_SECRET=${WEBHOOK_SECRET} -e PRIVATE_KEY_ENCODED=${PRIVATE_KEY_ENCODED}
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,29 @@
 import {Probot} from "probot";
 // import setup from "probot/lib/apps/setup";
 import app from "./app";
-
-const probot = new Probot(
-    {
+interface IAppConfig {
+    secret: string;
+    id: number;
+    cert: string;
+}
+const getAppConfig = (): IAppConfig => {
+    if (!process.env.APP_ID) {
+        throw new Error("APP_ID not set");
+    }
+    if (!process.env.WEBHOOK_SECRET) {
+        throw new Error("WEBHOOK_SECRET not set");
+    }
+    if (!process.env.PRIVATE_KEY_ENCODED) {
+        throw new Error("PRIVATE_KEY_ENCODEDs not set");
+    }
+    const id = parseInt(process.env.APP_ID, 10);
+    return {
+        id,
         secret: process.env.WEBHOOK_SECRET,
-        id: 32985,
-        port: 3000,
-        cert: process.env.KEY
-    });
+        cert: Buffer.from(process.env.PRIVATE_KEY_ENCODED, "base64").toString("ascii")
+    };
+};
+const probot = new Probot(getAppConfig());
 // probot.load(setup);
 probot.load(app);
 
